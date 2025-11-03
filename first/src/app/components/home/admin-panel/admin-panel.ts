1219aede449933649c0b24df10c576b8e3928511
Abakarov Ramazan <163338140+AbakarovR@users.noreply.github.com>
2025-11-04 01:29:35 +0300

    Update

--- first/src/app/components/home/admin-panel/admin-panel.ts
+++ first/src/app/components/home/admin-panel/admin-panel.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
     selector: 'app-admin-panel',
-    imports: [MatToolbarModule, MatIconModule, RouterLinkActive, RouterLink],
+    imports: [MatToolbarModule, MatIconModule, RouterLinkActive, RouterLink, MatButtonModule],
     templateUrl: './admin-panel.html',
     styleUrls: ['./admin-panel.scss']
 })
